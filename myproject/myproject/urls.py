aae98c099dba826caf879d57826a0b9b10a14574
Sangreal8 <joshmalone@hotmail.com>
2018-08-02 16:10:37 +0100

    initiated boards app

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from boards import views
+
+#url config was changed so path is used instead of url now
+#different syntax
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home)
 ]
